aa9d561d70dfd0696bfea5583b3139d90962bfb5
Thiluck Vardhan Vemula <youremail@example.com>
2026-01-19 20:10:04 +0530

    Conversion

---
+++ Classroom/Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Conversion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("---------------Conversion----------------");
+            System.Console.Write("Enter Feet/Foot to Convert to Centimeters:");
+            if(!int.TryParse(Console.ReadLine(),out int feet))
+            {
+                System.Console.WriteLine("Please Enter only Integer as input");
+            }
+            double centi=feet*30.48;
+            System.Console.WriteLine($"{centi:F2}");
+        }
+    }
+}
