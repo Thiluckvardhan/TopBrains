bc6839ecb5c4ad8c7b23d5ed7a96c1ff66a51064
Thiluck Vardhan Vemula <youremail@example.com>
2026-01-19 14:12:39 +0530

    Four Questions Completed

--- Classroom/StringFormat/Program.cs
+++ Classroom/StringFormat/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+namespace StringFormat
+{
+    public class Student
+    {
+        public string? Name { get; set; }
+        public int Score { get; set; }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new();
+            string[] items =
+            {
+            "Thiluck:80",
+            "Divyansh:79",
+            "Aarjav:39",
+            "Sumanth:80",
+            "Harsha:45",
+            // "Unknown"
+            };
+            System.Console.WriteLine("Enter Minimum Score");
+            if (!int.TryParse(Console.ReadLine(), out int MinScore))
+            {
+                System.Console.WriteLine("Please Enter only Integer as Input");
+                return;
+            }
+            foreach (var item in items)
+            {
+                Student stu = new();
+                var student = item.Split(':');
+                int temp;
+                if (student.Length != 2)
+                {
+                    Console.WriteLine("Input is Wrong");
+                    return;
+                }
+                else if (!int.TryParse(student[1], out temp))
+                {
+                    Console.WriteLine("Score is not integer skipping this student");
+                    continue;
+                }
+                stu.Name = student[0];
+                stu.Score = temp;
+                students.Add(stu);
+            }
+            var output = students.Where(s => s.Score >= MinScore).OrderByDescending(s => s.Score).ThenBy(s => s.Name).ToList();
+            var jsonformat = JsonSerializer.Serialize(output, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            Console.WriteLine(jsonformat);
+        }
+    }
+}
