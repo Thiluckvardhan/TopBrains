b8db162421713dbb32abaff03941f074d68b38b0
Thiluck Vardhan Vemula <youremail@example.com>
2026-01-19 20:51:13 +0530

    count characters

--- Classroom/Conversion/Program.cs
+++ Classroom/Conversion/Program.cs
@@ -9,9 +9,10 @@
             if(!int.TryParse(Console.ReadLine(),out int feet))
             {
                 System.Console.WriteLine("Please Enter only Integer as input");
+                return;
             }
             double centi=feet*30.48;
-            System.Console.WriteLine($"{centi:F2}");
+            System.Console.WriteLine($"{feet} in centimeters {Math.Round(centi,2,MidpointRounding.AwayFromZero)}cm");
         }
     }
 }
---
+++ Classroom/CountCharacterfromString/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CountCharacterfromString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("-----------------String Counter-------------------");
+            System.Console.WriteLine("Enter an Input to check the Count of its characters");
+            string? input = Console.ReadLine();
+            if (input == null)
+            {
+                Console.WriteLine("Your Input is Empty");
+                return;
+            }
+            SortedDictionary<char, int> stringcounter = new();
+            foreach (char letter in input)
+            {
+                stringcounter[letter] = stringcounter.ContainsKey(letter) ? stringcounter[letter] + 1 : 1;
+            }
+
+            System.Console.WriteLine("Character Count of your Input: ");
+            foreach (var item in stringcounter)
+            {
+                System.Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
