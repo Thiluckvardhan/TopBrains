fe38367a70784c79c843cfa84c9c2bee4d240058
Thiluck Vardhan Vemula <youremail@example.com>
2026-01-19 23:20:07 +0530

    CustomSorting

---
+++ Classroom/CustomSorting/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CustomSorting
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Marks { get; set; }
+    }
+
+    public class StudnetComparer : IComparer(Studnet)
+    {
+        
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Name = "Akhil", Age = 21, Marks = 95 },
+                new Student { Name = "Bhanu", Age = 19, Marks = 95 },   
+                new Student { Name = "Charan", Age = 20, Marks = 88 },
+                new Student { Name = "Divya", Age = 18, Marks = 88 },   
+                new Student { Name = "Esha", Age = 22, Marks = 99 },
+                new Student { Name = "Farhan", Age = 17, Marks = 75 },
+                new Student { Name = "Ganesh", Age = 19, Marks = 99 }, 
+                new Student { Name = "Harsha", Age = 20, Marks = 60 }
+            };
+            List<Student>result=students.OrderByDescending(s=>s.Marks).ThenBy(s=>s.Age).ToList();
+
+            foreach(Student item in result)
+            {
+                System.Console.WriteLine($"Name: {item.Name} Age: {item.Age}  Marks: {item.Marks}");
+            }
+        }
+    }
+}
